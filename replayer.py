abe819d2746ad87f72da7eb874917ee1082eea6c
VeraWangYawei <1163237220@qq.com>
2025-04-09 16:13:04 +0800

    update replayer for position

--- replayer.py
+++ replayer.py
@@ -1,6 +1,9 @@
 from utils.logger_setup_data_extraction import logger
 from replay_agent.planner.snapshot_analyzer import SnapshotAnalyzer
 from replay_agent.position_finder.position_extractor import PositionExtractor
+from datetime import datetime
+import os
+
 
 
 def run():
@@ -8,11 +11,14 @@ def run():
     
     # Step 1: Screen Snapshot and analyst in knowledge base (Error Handing)
     logger.info(f"\nstarted ScreenSnapshotAnalyzer...") 
-    snapshot_analyzer = SnapshotAnalyzer()
-    action = snapshot_analyzer.generate_action()
+    current_time_str = datetime.now().strftime('%m%d%H%M%S')
+    image_folder_path = os.path.join("input", "screenshot", current_time_str)
+    os.makedirs(image_folder_path)
+    # snapshot_analyzer = SnapshotAnalyzer(image_folder_path)
+    # action = snapshot_analyzer.generate_action()
 
-    # position_extractor = PositionExtractor()
-    # position_extractor.get_position_by_name("input\\screenshot\\screenshot.png","cell row 2, column A")
+    position_extractor = PositionExtractor()
+    position_extractor.get_position_by_name(image_folder_path, "input\\screenshot\\screenshot.png","Formula Bar","input_box")
 
     # Step 2: Action parser and take action
     # Step 3: Validation Success (Error Handing)
