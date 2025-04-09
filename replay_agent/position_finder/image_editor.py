758a243998ce0ab631d58896f29579cbb753e4c9
VeraWangYawei <1163237220@qq.com>
2025-04-09 16:06:14 +0800

    add position extractor

--- replay_agent/position_finder/image_editor.py
+++ replay_agent/position_finder/image_editor.py
@@ -11,6 +11,72 @@ class ImageEditor:
         if self.image is None:
             raise ValueError(f"Unable to load image, please check the path: {self.image_path}")
         
+    def mark_text_box(self, save_path):
+        self.load_image()
+        gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+        
+        thresh = cv2.adaptiveThreshold(
+            blurred, 255,
+            cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+            cv2.THRESH_BINARY_INV, 11, 2
+        )
+        
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 1))
+        closed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+        
+        contours, _ = cv2.findContours(closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        input_boxes = []
+        i = 0
+        
+        for cnt in contours:
+            peri = cv2.arcLength(cnt, True)
+            approx = cv2.approxPolyDP(cnt, min(30, peri * 0.05), True)
+            (x, y, w, h) = cv2.boundingRect(approx)
+
+            if len(approx) == 4 and w > 30 and h > 30:
+                input_boxes.append((x, y, w, h))
+                i += 1
+
+        box_number = 0
+
+        for (x, y, w, h) in input_boxes:
+            cv2.rectangle(self.image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            
+            circled_char = str(box_number) 
+            
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            font_scale = 0.8
+            text_color = (0, 0, 255)  
+            circle_color = (0, 0, 255)  
+            thickness = 2
+            
+            (text_w, text_h), _ = cv2.getTextSize(circled_char, font, font_scale, thickness)
+            radius = max(text_w, text_h) // 2 + 5 
+            text_x = x 
+            text_y = y + h - radius - 15  
+            
+            # cv2.circle(
+            #     img,
+            #     (text_x + radius, text_y + radius),
+            #     radius,
+            #     circle_color,
+            #     2  # 填充
+            # )
+            
+            cv2.putText(
+                self.image, circled_char,
+                (text_x + radius - text_w//2, text_y + radius + text_h//2),
+                font, font_scale,
+                text_color, thickness,
+                cv2.LINE_AA
+            )
+
+            box_number += 1
+            
+        cv2.imwrite(save_path, self.image)
+        return input_boxes
+    
     def add_star(self, x, y):
         self.load_image()
         center = (x, y)
@@ -87,14 +153,12 @@ class ImageEditor:
         return save_path
     
     def draw_grid_and_labels(self, save_path, label_offset):
-        
-        
         self.load_image()
 
         height, width = self.image.shape[:2]
 
         alpha = 0.20 
-        cols = 40
+        cols = 35
         rows = 55
         resize_width = width // 2
         resize_height = height // 2
@@ -109,14 +173,19 @@ class ImageEditor:
         
         overlay = gray_bgr.copy()
 
+        num_cols = cols
+        num_rows = rows
+
         
-        for i in range(rows + 1):
-            for j in range(cols + 1):
-                center_x = int(j * stepx + stepx // 2 )
-                center_y = int(i * stepy + stepy // 2 )
-                label = f"{j + i * cols}"
-                cv2.putText(overlay, label, (center_x, center_y),
-                            cv2.FONT_HERSHEY_SIMPLEX, font_size, (255, 0, 0), thickness=1, lineType=cv2.LINE_AA)
+        for i in range(num_rows+3):
+            for j in range(num_cols+3):
+                center_x = int(j * stepx + stepx // 2 - label_offset * 5)
+                center_y = int(i * stepy + stepy // 2 - label_offset * 5)
+                label = f"{j + i * num_cols}"
+                cv2.putText(overlay, label, (center_x - 3, center_y + 1),
+                            cv2.FONT_HERSHEY_SIMPLEX, font_size, (255, 255, 255), thickness=1, lineType=cv2.LINE_AA)
+                # if(j + i * cols == 621) :
+                #     print(i)
         
         final_image = cv2.addWeighted(overlay, alpha, gray_bgr, 1 - alpha, 0)
         
@@ -134,13 +203,40 @@ class ImageEditor:
         stepx = width / cols
         stepy = height / rows
 
-        i = grid_cell // cols
-        j = grid_cell % cols
+        num_cols = cols
+        # num_rows = rows + 3
 
-        center_x = int(j * stepx + stepx // 2 )
-        center_y = int(i * stepy + stepy // 2 )
+        i = grid_cell // num_cols
+        j = grid_cell % num_cols
+
+        center_x = int(j * stepx + stepx // 2 - label_offset * 10)
+        center_y = int(i * stepy + stepy // 2 - label_offset * 10)
 
         return center_x, center_y
+    
+    def add_X(self, x, y, radius, save_path):
+        # 重新加载原图，防止叠加绘制
+        self.load_image()
+        
+        # 红色在 BGR 格式中表示为 (0, 0, 255)
+        red = (0, 0, 255)
+        thickness = 2  # 可根据需要调整线宽
+
+        # 绘制从左上到右下的对角线
+        cv2.line(self.image, (x - radius, y - radius), (x + radius, y + radius), red, thickness)
+        # 绘制从左下到右上的对角线
+        cv2.line(self.image, (x - radius, y + radius), (x + radius, y - radius), red, thickness)
+        
+        height, width = self.image.shape[:2]
+        resize_width = width 
+        resize_height = height 
+
+        img_resize  = cv2.resize(self.image,(resize_width,resize_height))
+
+        cv2.imwrite(save_path, img_resize)
+
+        return save_path
+
 
 
 
--- replay_agent/position_finder/position_extractor.py
+++ replay_agent/position_finder/position_extractor.py
@@ -1,45 +1,154 @@
 from core.llm_chat import LLMChat
 from replay_agent.planner.replayer_action import ReplayerAction
 from replay_agent.position_finder.image_editor import ImageEditor
-from prompt.action_generator import VARIFY_POSITION
+from prompt.action_generator import VERIFY_POSITION
 from prompt.action_generator import GET_LABELS
-
+from prompt.action_generator import IDENTIFY_TEXT_BOX_LABELS
 import re
 from utils.logger_setup_data_extraction import logger
 from utils.json_processor import json_process
+from replay_agent.planner.snapshot_analyzer import SnapshotAnalyzer
+
+import pyautogui
+import os
+import time
 
 
 class PositionExtractor:
     def __init__(self):      
         self.chat = LLMChat() 
 
+    def get_position_by_name(self, image_folder_path, image_path, element_name, element_type):
+        if element_type =="input_box":
+            x,y = self.get_position_input_box(image_folder_path, image_path, element_name)
+
+        elif element_type =="cell":
+            x,y = self.get_position_cell(image_folder_path, image_path, element_name)
+
+        elif element_type =="text_button":
+            x,y = self.get_position_text_button(image_folder_path, image_path, element_name)
+
+        return x,y
     
-    def get_position_by_name(self, image_path, element_name):
+    def get_position_input_box(self, image_folder_path, image_path, element_name):
+        
+        imageEditor = ImageEditor(image_path)
+
+        save_path_mark_text_box = os.path.join (image_folder_path, element_name + "_mark_text_box.png")
+
+        input_boxes = imageEditor.mark_text_box(save_path_mark_text_box)
+
+        index = 0
+
+        while index < 3:
+
+            identify_text_box_labels = IDENTIFY_TEXT_BOX_LABELS.format(element_name = element_name)
+
+            replayerAction = ReplayerAction(self.chat)
+
+            text_box_labels_reply = replayerAction.get_position_gemini(save_path_mark_text_box, identify_text_box_labels)
+
+            text_box_labels_reply_json = json_process(text_box_labels_reply)
+
+            number = int(text_box_labels_reply_json[element_name]["number"])
+
+            x, y, w, h = input_boxes[number]
+            e_x = x + w//2
+            e_y = y + h//2
+
+            position_verify = self.verify_position(e_x, e_y, element_name, image_folder_path, image_path, str(number)+"_"+str(index))
+
+            if position_verify:
+                break
+            else:
+                index += 1
+
+        return e_x, e_y
+    
+    def get_position_cell(self, image_folder_path, image_path, element_name):
+        
+        imageEditor = ImageEditor(image_path)
+
+        save_path_mark_text_box = os.path.join (image_folder_path, element_name + "_mark_text_box.png")
+
+        input_boxes = imageEditor.mark_text_box(save_path_mark_text_box)
+        
+
+        return 1,1
+    
+    def get_position_text_button(self, image_folder_path, image_path, element_name):
+        return 1,1
+    
+    def verify_position(self, x, y, element_name, image_folder_path, image_path, number):
+
+        verify_position_path = os.path.join(image_folder_path, element_name + "_verify_position_"+ str(number) +".png")
+        
+        imageEditor = ImageEditor(image_path)
+
+        # snapshot_analyzer = SnapshotAnalyzer(image_folder_path)
+
+        verify_add_X_image = imageEditor.add_X(x, y, 5, verify_position_path)
+
+        # screen_concept = snapshot_analyzer.get_screen_snapshot(verify_posision_path)
+
+        verify_position = VERIFY_POSITION.format(element_name = element_name)
+
+        replayerAction = ReplayerAction(self.chat)
+
+        verify_position_reply = replayerAction.get_position_gemini(verify_add_X_image, verify_position)
+
+        verify_position_reply_json = json_process(verify_position_reply)
+
+        position_verify_result = verify_position_reply_json["position"]
+
+        if position_verify_result == "inside":
+            logger.info("verified coordinate:(" + str(x) + "," + str(y) + ")")
+            return True
+        logger.info("error coordinate:(" + str(x) + "," + str(y) + ")")
+        return False
+    
+    def get_position_by_name_0(self, image_folder_path, image_path, element_name):
 
         pattern = r"cell\s+([A-Za-z]+)(\d+)"
         match = re.search(pattern, element_name)
+
+        verfy_time = 0
 
         if match:
             letter = match.group(1)  
             number = match.group(2)
             if letter > 'A' and number > '1':
-                first_row_x = self.get_position_by_label(image_path, "cell " + letter +"1")[0]
-                first_col_y = self.get_position_by_label(image_path, "cell A" + number)[1]
-                logger.info(element_name + "accurate coordinate:(" + str(first_row_x) + "," + str(first_col_y) + ")")
+                while verfy_time < 2:
+
+                    first_row_x = self.get_position_by_label(image_folder_path, image_path, "cell " + letter +"1", verfy_time)[0]
+                    first_col_y = self.get_position_by_label(image_folder_path, image_path, "cell A" + number, verfy_time)[1]
+
+                    logger.info(element_name + "accurate coordinate:(" + str(first_row_x) + "," + str(first_col_y) + ")")
+
+                    position_verify_result = self.action_and_verify_position(first_row_x, first_col_y, element_name, "cell_verfy_" + str(verfy_time), str(first_row_x) + "_" + str(first_col_y), image_folder_path, image_path)
+
+                    if position_verify_result:
+                        return first_row_x , first_col_y
+                    else:
+                        verfy_time = verfy_time + 1
+
                 return first_row_x, first_col_y
             
-        return self.get_position_by_label(image_path, element_name)
+            
+        return self.get_position_by_label(image_folder_path, image_path, element_name, verfy_time)
     
 
-    def get_position_by_label(self, image_path, element_name):
+    def get_position_by_label(self, image_folder_path, image_path, element_name, verfy_time):
 
         imageEditor = ImageEditor(image_path)
 
-        label_offset = 0
+        label_find_time = 0
 
-        while label_offset < 2 :
+        while label_find_time < 5 :
 
-            save_path_number_labels = "input\\screenshot\\mark_number_labels.png"
+            label_offset = label_find_time + verfy_time * 5
+
+            save_path_number_labels = os.path.join (image_folder_path, element_name + "_mark_number_labels_"+ str(label_offset) +".png")
 
             label_cols, label_rows = imageEditor.draw_grid_and_labels(save_path_number_labels, label_offset)
 
@@ -49,7 +158,7 @@ class PositionExtractor:
             
             labels_reply = replayerAction.get_position_gemini(save_path_number_labels, get_labels)
 
-            # varify
+            # verify
 
             labels_reply_json = json_process(labels_reply)
 
@@ -57,27 +166,47 @@ class PositionExtractor:
 
             center_x, center_y = imageEditor.labels_to_position(label_cols, label_rows, grid_cell, label_offset)
 
-            radius_varify = 3
+            position_verify = self.action_and_verify_position(center_x, center_y, element_name, label_offset, grid_cell, image_folder_path, image_path)
 
-            image_varify = "input\\screenshot\\mark_varify.png"
-
-            add_circle_image_varify = imageEditor.add_circle(center_x, center_y, radius_varify, image_varify)
-
-            varify_position = VARIFY_POSITION.format(element_name = element_name)
-            varify_position_reply = replayerAction.get_position_gemini(image_varify, varify_position)
-
-            varify_position_reply_json = json_process(varify_position_reply)
-            position_varify = varify_position_reply_json[element_name]["RGB_element completely inside " + element_name]
-
-
-            if position_varify == "yes":
-                print("varified coordinate:(" + str(center_x) + "," + str(center_y) + ")")
+            if position_verify:
                 return center_x ,center_y
             else:
-                label_offset = label_offset + 1
+                label_find_time = label_find_time + 1
         
 
         return center_x ,center_y
+    
+    def action_and_verify_position(self, x, y, element_name, label_offset, grid_cell, image_folder_path, image_path):
+
+        verify_posision_path = os.path.join(image_folder_path, element_name + "_click_position_"+ str(label_offset) + "_" + str(grid_cell) + ".png")
+        
+        imageEditor = ImageEditor(image_path)
+
+        # snapshot_analyzer = SnapshotAnalyzer(image_folder_path)
+
+        verify_add_X_image = imageEditor.add_X(x, y, 5, verify_posision_path)
+
+        # screen_concept = snapshot_analyzer.get_screen_snapshot(verify_posision_path)
+
+        verify_position = VERIFY_POSITION.format(element_name = element_name)
+
+        replayerAction = ReplayerAction(self.chat)
+
+        verify_position_reply = replayerAction.get_position_gemini(verify_add_X_image, verify_position)
+
+        verify_position_reply_json = json_process(verify_position_reply)
+
+        position_verify_result = verify_position_reply_json["position"]
+
+        if position_verify_result == "inside":
+            logger.info("verified coordinate:(" + str(x) + "," + str(y) + ")")
+            return True
+        logger.info("error coordinate:(" + str(x) + "," + str(y) + ")")
+        return False
+    
+
+
+
 
 
 
